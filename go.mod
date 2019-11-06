bec15ec9f2b90ad4c9f12b12411444866337d3b2
xiangzhi <xiangzhi@126.com>
2019-11-06 11:51:25 +0800

    新增搜索

--- go.mod
+++ go.mod
@@ -1,20 +1,31 @@
 module github.com/xinzf/xunray
 
-go 1.12
+go 1.13
 
 require (
-	github.com/gin-gonic/gin v1.4.0
+	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
+	github.com/gin-gonic/gin v1.3.0
+	github.com/go-ego/gse v0.0.0-20191031151436-7129ddefdf69 // indirect
+	github.com/go-ego/riot v0.0.0-20190802171934-6ed3775d67b6
 	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/go-xorm/xorm v0.7.9
 	github.com/gofrs/uuid v3.2.0+incompatible
+	github.com/google/btree v1.0.0 // indirect
 	github.com/hashicorp/consul/api v1.2.0
-	github.com/json-iterator/go v1.1.7
+	github.com/json-iterator/go v1.1.8
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mattn/go-isatty v0.0.7 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/shopspring/decimal v0.0.0-20191009025716-f1972eb1d1f5
 	github.com/sirupsen/logrus v1.4.2
-	github.com/spf13/viper v1.4.0
+	github.com/spf13/viper v1.3.2
 	github.com/toolkits/file v0.0.0-20160325033739-a5b3c5147e07
 	github.com/toolkits/net v0.0.0-20160910085801-3f39ab6fe3ce
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+	gopkg.in/yaml.v2 v2.2.4 // indirect
 	xorm.io/core v0.7.2
 )
---
+++ storage/searcher.go
@@ -0,0 +1,164 @@
+package storage
+
+import (
+	"fmt"
+	"github.com/go-ego/riot"
+	"github.com/go-ego/riot/types"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"strconv"
+)
+
+type Searcher struct {
+	engine *riot.Engine
+}
+
+func (Searcher) Init(name string) *Searcher {
+	searcher := new(Searcher)
+	searcher.engine = &riot.Engine{}
+	searcher.engine.Init(types.EngineOpts{
+		Model:     "zh",
+		Using:     4,
+		NotUseGse: true,
+		//GseMode: true,
+		//GseDict: viper.GetString("searcher.dict_path"),
+		GseDict: viper.GetString(fmt.Sprintf("searcher.%s.dict_path", name)),
+		IndexerOpts: &types.IndexerOpts{
+			IndexType: types.DocIdsIndex,
+		},
+		UseStore: true,
+		//StoreFolder: viper.GetString("searcher.store_path"),
+		StoreFolder: viper.GetString(fmt.Sprintf("searcher.%s.store_path", name)),
+		StoreEngine: "bg", // bg: badger, lbd: leveld
+	})
+	searcher.engine.Flush()
+	logrus.Infoln("Searcher: " + name + " Init")
+	return searcher
+	//go func() {
+	//	ticker := time.NewTicker(time.Duration(10) * time.Second)
+	//	defer ticker.Stop()
+	//	for {
+	//		select {
+	//		case <-ticker.C:
+	//			this.engine.Flush()
+	//			fmt.Println("Search Flush...")
+	//		}
+	//	}
+	//}()
+}
+
+func (this *Searcher) RemoveIndex(applyId int) {
+	go func() {
+		this.engine.RemoveDoc(strconv.Itoa(applyId), true)
+	}()
+}
+
+func (this *Searcher) Indexer(applyId int, text string) {
+	this.engine.Index(strconv.Itoa(applyId), types.DocData{Content: text}, true)
+	this.engine.Flush()
+}
+
+//func (this *Searcher) SearchApplyByFields(data map[string]string, fields []string, size int) (applyIds []int) {
+//	applyIds = make([]int, 0)
+//
+//	fmt.Println("进入冲突查询函数")
+//	_search := func(key, val string, wg *sync.WaitGroup) {
+//		defer wg.Done()
+//
+//		result := this.engine.Search(types.SearchReq{
+//			Text: fmt.Sprintf("%s %s", key, val),
+//			RankOpts: &types.RankOpts{
+//				OutputOffset: 0,
+//				MaxOutputs:   size,
+//			},
+//			Logic: types.Logic{
+//				Must: true,
+//			},
+//		})
+//
+//		ids := make([]int, 0)
+//		if result.Docs.(types.ScoredDocs).Len() > 0 {
+//			for _, d := range result.Docs.(types.ScoredDocs) {
+//				_id, _ := strconv.Atoi(d.DocId)
+//				ids = append(ids, _id)
+//				applyIds = append(applyIds, _id)
+//			}
+//		}
+//		fmt.Println(key, val, ids)
+//	}
+//
+//	inArray := func(str string, strs []string) bool {
+//		for _, s := range strs {
+//			if s == str {
+//				return true
+//			}
+//		}
+//		return false
+//	}
+//
+//	fieldNum := 0
+//	var wg sync.WaitGroup
+//	for key, val := range data {
+//		if val == "" {
+//			continue
+//		}
+//		if !inArray(key, fields) {
+//			continue
+//		}
+//
+//		fieldNum++
+//		wg.Add(1)
+//		_search(key, val, &wg)
+//	}
+//	wg.Wait()
+//
+//	if len(applyIds) == 0 || fieldNum == 0 {
+//		fmt.Println("没有找到冲突数据")
+//		return
+//	}
+//
+//	scores := make(map[int]int)
+//	for _, id := range applyIds {
+//		times, found := scores[id]
+//		if !found {
+//			times = 0
+//		}
+//		times++
+//		scores[id] = times
+//	}
+//
+//	fmt.Println("scores:", scores)
+//
+//	applyIds = make([]int, 0)
+//	for id, times := range scores {
+//		rate := float64(times) / float64(fieldNum)
+//		fmt.Println("Total:", float64(fieldNum), "Times:", float64(times), "Rate:", rate, "score:", viper.GetFloat64("searcher.apply_score"))
+//		if rate >= viper.GetFloat64("searcher.apply_score") {
+//			applyIds = append(applyIds, id)
+//		}
+//	}
+//
+//	fmt.Println("applyIds", applyIds)
+//	return
+//}
+
+func (this *Searcher) Search(keywords string, offset, size int) ([]int, int) {
+	result := this.engine.Search(types.SearchReq{
+		Text: keywords,
+		RankOpts: &types.RankOpts{
+			OutputOffset: offset,
+			MaxOutputs:   size,
+		},
+	})
+
+	docIds := make([]int, 0)
+	docs := result.Docs.(types.ScoredDocs)
+	if docs.Len() > 0 {
+		for _, d := range docs {
+			_id, _ := strconv.Atoi(d.DocId)
+			docIds = append(docIds, _id)
+		}
+	}
+
+	return docIds, result.NumDocs
+}
